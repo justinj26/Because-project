59de32fd24dc7a33d57e35198aada8ebe9c50e3d
justin_james <jjames11799@gmail.com>
2020-06-29 17:50:28 -0400

    monday work

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = "because"
+    config.api_key = "658949264217798"
+    config.api_secret = "-w43kor2QvGHlxFAuEFuK0SHgRQ"
+    config.secure = true
+    config.cdn_subdomain = true 
+end
---
+++ test/models/hash_post_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HashPostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
