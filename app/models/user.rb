64c02cd06bb9ad838151fafee5a99e69bf41d9ce
mlkelly <maurak87@gmail.com>
2020-06-30 17:24:08 -0400

    model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,24 @@ class User < ApplicationRecord
 
     has_secure_password
 
+    # calculates the account age of the user
     def account_age
+        # gives the date account was created
+            # created = @user.created_at.strftime("%m/%d/%y").split("/").map(&:to_i)
+            # today = Date.today.strftime("%m/%d/%y").split("/").map(&:to_i)
+            # created_array= []
+            # month = today[0] - created[0]
+            # day = today[1] - created[1]
+            # year = today[2] - created[2]
+        days = DateTime.now.mjd - created_at.to_date.mjd 
+        year = days/365
+        remaining_days = days%365
+        "This account is #{year} years and #{remaining_days} days old."
     end
 
+    # displays the users most used hashtags on users/show
     def most_used_hashtags
+        
     end
 
     def posts_in_order
