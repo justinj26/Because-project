0b855454d0f1161080f87a2a4d47af7491ea9203
justin_james <jjames11799@gmail.com>
2020-07-01 16:50:25 -0400

    show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,23 +11,6 @@ Comment.destroy_all
 Hashtag.destroy_all
 HashPost.destroy_all
 
-post1 = Post.create(content: "Wow, this is my first post")
-post2 = Post.create(content: "Wow, this is my second post")
-post3 = Post.create(content: "Wow, this is my third post")
-post4 = Post.create(content: "Wow, this is my fourt post")
-
-comment1 = Comment.create(message: "Oh my gosh, did you see what was said, that was so thoughtful!", post_id: post4.id)
-comment2 = Comment.create(message: "I really like how they structured out that argument, so organize.", post_id: post2.id)
-comment3 = Comment.create(message: "I think the reason for that is to find a closer meaning in life", post_id: post1.id)
-
-h1 = Hashtag.create(name: "Because_ilikeit")
-h2 = Hashtag.create(name: "Because_whynot")
-h3 = Hashtag.create(name: "Because_yes")
-
-hp1 = HashPost.create(post_id: post1.id, hashtag_id: h1.id)
-hp2 = HashPost.create(post_id: post2.id, hashtag_id: h1.id)
-hp3 = HashPost.create(post_id: post3.id, hashtag_id: h2.id)
-
 e1 = Emoticon.create(name:"love", image:"(ɔ◔‿◔)ɔ ♥")
 e2 = Emoticon.create(name:"flipping table", image:"┻━┻ ︵ヽ(`▭´)ﾉ︵﻿ ┻━┻")
 e3 = Emoticon.create(name:"peace", image:"(>‿◠)✌")
@@ -58,6 +41,25 @@ e27 = Emoticon.create(name:"sleeping", image:"(＿ ＿*) Z z z")
 e28 = Emoticon.create(name:"cat", image:"(^◕ᴥ◕^)")
 e29 = Emoticon.create(name:"dog", image:"V●ᴥ●V")
 e30 = Emoticon.create(name:"rabbit", image:"／(=´x`=)＼")
+
+post1 = Post.create(content: "Wow, this is my first post", emoticon_id: , image_id: )
+post2 = Post.create(content: "Wow, this is my second post", emoticon_id: , image_id: )
+post3 = Post.create(content: "Wow, this is my third post", emoticon_id: , image_id: )
+post4 = Post.create(content: "Wow, this is my fourt post", emoticon_id: , image_id: )
+
+comment1 = Comment.create(message: "Oh my gosh, did you see what was said, that was so thoughtful!", post_id: post4.id)
+comment2 = Comment.create(message: "I really like how they structured out that argument, so organize.", post_id: post2.id)
+comment3 = Comment.create(message: "I think the reason for that is to find a closer meaning in life", post_id: post1.id)
+
+h1 = Hashtag.create(name: "Because_ilikeit")
+h2 = Hashtag.create(name: "Because_whynot")
+h3 = Hashtag.create(name: "Because_yes")
+
+hp1 = HashPost.create(post_id: post1.id, hashtag_id: h1.id)
+hp2 = HashPost.create(post_id: post2.id, hashtag_id: h1.id)
+hp3 = HashPost.create(post_id: post3.id, hashtag_id: h2.id)
+
+
 
 
 
